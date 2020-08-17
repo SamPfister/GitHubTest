56040949e7957bb22c10c0b4a5810e3bdcf334b5
SamPfister <54378666+SamPfister@users.noreply.github.com>
2020-08-17 08:22:22 -0500

    first
    this is the first commit

---
+++ Assets/Scripts/Speen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class Speen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(30, 30, 30) * Time.deltaTime * 10);
+    }
+}
